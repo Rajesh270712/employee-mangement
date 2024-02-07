b07f08d5da93d0071545599654b094aacd153045
Rajesh Bagul <rajeshbagul2001@gmail.com>
2024-02-07 14:00:36 +0530

    minor change

--- src/trackingScript.js
+++ src/trackingScript.js
@@ -1,3 +1,5 @@
+// yourScript.js
+
 const triggerTrackingEvents = () => {
   const config = {
     url: 'https://analytics.synthaxia.com/v1/measurement/analytics'
@@ -6,20 +8,17 @@ const triggerTrackingEvents = () => {
   window.PhylloConnect.initialize(config);
 }
 
-// Create script element
-const script = document.createElement('script');
-script.type = 'text/javascript';
-script.async = true
-script.src = 'https://cdn.dev.getphyllo.com/connect/v2/phyllo-connect.js';
+function loadScript(src, callback) {
+  const script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = src;
+  script.onload = callback;
 
-// Set up the callback function directly on the script element
-script.onload = function() {
-  triggerTrackingEvents();
-};
+  document.body.appendChild(script);
+}
 
-// Append script element to the body
-document.body.appendChild(script);
+// Load the Phyllo Connect script and call triggerTrackingEvents when it's loaded
+loadScript('https://cdn.dev.getphyllo.com/connect/v2/phyllo-connect.js', triggerTrackingEvents);
 
 // Call triggerTrackingEvents when the window location changes
 window.addEventListener('hashchange', triggerTrackingEvents);
-
